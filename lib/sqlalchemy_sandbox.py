c01b246e6e3c4cd7a12ac12d145d882a95461d5c
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-24 19:09:03 +0300

    Done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,10 +12,158 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12')
+    )
 
-    id = Column(Integer(), primary_key=True)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
+
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    # students = session.query(Student)
+
+    # print([student for student in students])
+
+    # .all() option
+
+    students = session.query(Student).all()
+
+    print(students)
+    # selecting certain columns --> name
+    names = [name for name in session.query(Student.name)]
+
+    print(names)
+
+    #ORDERING
+
+    students_by_name = [student for student in session.query(
+            Student.name).order_by(
+            Student.name)]
+
+    print(students_by_name)
+    #ORDERING IN DESCENDING ORDER
+    students_by_grade_desc = [student for student in session.query(
+            Student.name, Student.grade).order_by(
+            desc(Student.grade))]
+
+    print(students_by_grade_desc)
+
+    #To limit your result set to the first x records, you can use the limit() method:
+
+    oldest_student = [student for student in session.query(
+            Student.name, Student.birthday).order_by(
+            desc(Student.grade)).limit(1)]
+
+    print(oldest_student)
+    
+    #FUNC 
+    student_count = session.query(func.count(Student.id)).first()
+
+    print(student_count)
+
+    #filtering
+
+    query = session.query(Student).filter(Student.name.like('%Alan%'),
+        Student.grade == 11)
+
+    for record in query:
+        print(record.name)
+
+    #updating data
+    # for student in session.query(Student):
+    #     student.grade += 1
+
+    # session.commit()
+
+    # print([(student.name,
+    #     student.grade) for student in session.query(Student)])
+    
+    #anada wan
+
+    session.query(Student).update({
+        Student.grade: Student.grade + 1
+    })
+
+    print([(
+        student.name,
+        student.grade
+    ) for student in session.query(Student)])
+
+    #DELETE METHOD --> BEWARE
+    # query = session.query(
+    #     Student).filter(
+    #         Student.name == "Albert Einstein")        
+
+    # # retrieve first matching record as object
+    # albert_einstein = query.first()
+
+    # # delete record
+    # session.delete(albert_einstein)
+    # session.commit()
+
+    # # try to retrieve deleted record
+    # albert_einstein = query.first()
+
+    # print(albert_einstein)
+    
+    #ANOTHER WAN
+    query = session.query(
+        Student).filter(
+            Student.name == "Albert Einstein")
+
+    query.delete()
+
+    albert_einstein = query.first()
+
+    print(albert_einstein)
